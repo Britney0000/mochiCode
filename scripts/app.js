80b0543061c5e22566c86f4286805d40cead1419
RBritney <britneyrangith@gmail.com>
2025-01-26 20:58:44 +0100

    Menu hamburger
    Quelques amélioration à faire

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    // Sélectionne le bouton hamburger et le menu déroulant
+    const toggleBtn = document.querySelector('.toggle_btn');
+    const dropdownContainer = document.querySelector('.dropdown_container');
+
+    // Vérifie si les éléments existent avant d'ajouter les événements
+    if (toggleBtn && dropdownContainer) {
+        toggleBtn.addEventListener('click', () => {
+            dropdownContainer.classList.toggle('show'); // Affiche/masque le menu
+        });
+    } else {
+        console.error("Éléments .toggle_btn ou .dropdown_container introuvables !");
+    }
+});
